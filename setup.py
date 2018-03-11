08ed5cb0ecab0ae16040dd1161e23e11176dc64d
Steffen Kockel <info@steffen-kockel.de>
2018-03-11 12:08:34 +0100

    packaging: add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Pyduin',
+      version='0.1',
+      description='Extensive ',
+      url='http://github.com/SteffenKockel/pyduin',
+      author='Steffen Kockel',
+      author_email='',
+      license='GPLv3',
+      packages=['pyduin'],
+      zip_safe=False)
