2e6803b2e6163e8fc24d39813199bdf527d6ad58
Steffen Kockel <info@steffen-kockel.de>
2023-09-23 14:54:34 +0200

    Fix pylint errors, start fixing the firmware test case
    The test case is intended to verify the firmware responses from a
    live arduino. The goal is a dynamic testbed to verify the respones
    of devices with firmware applied.

--- tests/test_arduino.py
+++ tests/test_arduino.py
@@ -1,121 +1,127 @@
 # -*- coding: utf-8 -*-
+""" Test Arduino firmware """
 
 import unittest
-from mock import patch
-import mock
-import sys
-from time import sleep
-from pyduin.arduino import Arduino
 from io import StringIO
 
+from mock import patch
+
+from pyduin import arduino
+from pyduin import _utils as utils
 
 CONFIG = {
     'tty': '/dev/ttyUSB0',
     'baudrate': '115200',
     'pinfile': 'tests/data/pinfiles/nano.yml',
-    'model': 'NanO' # On purpose. To test lowercase conversion
+    'board': 'nanoatmega328' 
 }
 
+# pylint: disable=missing-docstring
 class TestArduinoFirmwareMethods(unittest.TestCase):
 
     def setUp(self):
-        self.Arduino = Arduino(**CONFIG)
+        CONFIG['pinfile'] = utils.board_pinfile(CONFIG['board'])
+        self.arduino = arduino.Arduino(wait=True, **CONFIG)
 
     # def tearDown(self):
-    #     self.Arduino.Connection.close()
+    #     self.arduino.Connection.close()
 
     @patch('sys.stdout', new_callable=StringIO)
-    def test_connection(self, mock_stdout):
-        self.Arduino.open_serial_connection()
+    def test_connection(self, mock_stdout): # pylint: disable=unused-argument
+        self.arduino.open_serial_connection()
         # Test for "Boot complete" after pin setup
-        msg = self.Arduino.Connection.readline().strip()
+        def read():
+            return self.arduino.Connection.readline().strip().decode('utf-8')
+
+        msg = read()
         self.assertEqual(msg, "Boot complete")
 
-        # Test for pin setup response
-        expected = ["0%2%2\r\n0%3%1\r\n0%4%1\r\n0%5%2\r\n"+
-                    "0%6%2\r\n0%7%2\r\n0%8%2\r\n0%9%2\r\n"+
-                    "0%10%2\r\n0%11%2\r\n0%12%2\r\n0%13%1\r\n"+
-                    "0%14%0\r\n0%15%2\r\n0%16%0\r\n0%17%0\r\n"+
-                    "0%18%2\r\n0%19%2\r\n0%20%-1\r\n0%21%-1\r\n"]
-        msg = ""
-        for i in range(2,22):
-                msg += self.Arduino.Connection.readline()
-        self.assertEqual(msg, expected[0])
+        # # Test for pin setup response
+        # expected = ["0%2%2\r\n0%3%1\r\n0%4%1\r\n0%5%2\r\n"+
+        #             "0%6%2\r\n0%7%2\r\n0%8%2\r\n0%9%2\r\n"+
+        #             "0%10%2\r\n0%11%2\r\n0%12%2\r\n0%13%1\r\n"+
+        #             "0%14%0\r\n0%15%2\r\n0%16%0\r\n0%17%0\r\n"+
+        #             "0%18%2\r\n0%19%2\r\n0%20%-1\r\n0%21%-1\r\n"]
+        # msg = ""
+        # for i in range(2,22):
+        #     msg += self.arduino.Connection.readline().strip().decode('utf-8')
+        # self.assertEqual(msg, expected[0])
 
-        # Test for stdout
-        expected = ["Set pin mode for pin 2 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 3 to OUTPUT\n"+
-                    "Set pin mode for pin 4 to OUTPUT\n"+
-                    "Set pin mode for pin 5 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 6 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 7 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 8 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 9 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 10 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 11 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 12 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 13 to OUTPUT\n"+
-                    "Set pin mode for pin 14 to INPUT\n"+
-                    "Set pin mode for pin 15 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 16 to INPUT\n"+
-                    "Set pin mode for pin 17 to INPUT\n"+
-                    "Set pin mode for pin 18 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 19 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 20 to INPUT_PULLUP\n"+
-                    "Set pin mode for pin 21 to INPUT_PULLUP\n"
-                    ]
 
-        res = mock_stdout.getvalue()
-        self.assertEqual(res, expected[0])
+        # # Test for stdout
+        # expected = ["Set pin mode for pin 2 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 3 to OUTPUT\n"+
+        #             "Set pin mode for pin 4 to OUTPUT\n"+
+        #             "Set pin mode for pin 5 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 6 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 7 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 8 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 9 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 10 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 11 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 12 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 13 to OUTPUT\n"+
+        #             "Set pin mode for pin 14 to INPUT\n"+
+        #             "Set pin mode for pin 15 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 16 to INPUT\n"+
+        #             "Set pin mode for pin 17 to INPUT\n"+
+        #             "Set pin mode for pin 18 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 19 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 20 to INPUT_PULLUP\n"+
+        #             "Set pin mode for pin 21 to INPUT_PULLUP\n"
+        #             ]
 
-        # Test for pin mode OUTPUT
-        self.Arduino.Pins[13].Mode.output()
-        msg = self.Arduino.Connection.readline().strip()
-        self.assertEqual(msg, '0%13%1')
+        # res = mock_stdout.getvalue()
+        # self.assertEqual(res, expected[0])
 
-        # Test for pin mode INPUT
-        self.Arduino.Pins[13].Mode.input()
-        msg = self.Arduino.Connection.readline().strip()
-        self.assertEqual(msg, '0%13%0')
+        # # Test for pin mode OUTPUT
+        # self.arduino.Pins[13].Mode.output()
+        # msg = read()
+        # self.assertEqual(msg, '0%13%1')
 
-        # Test for pin mode INPUT_PULLUP}
-        self.Arduino.Pins[13].Mode.input_pullup()
-        msg = self.Arduino.Connection.readline().strip()
-        self.assertEqual(msg, '0%13%2')
+        # # Test for pin mode INPUT
+        # self.arduino.Pins[13].Mode.input()
+        # msg = read()
+        # self.assertEqual(msg, '0%13%0')
 
-        # Test for pin on
-        for pin in (4,7,8,12):
-            self.Arduino.Pins[pin].Mode.output()
-            msg = self.Arduino.Connection.readline().strip()
-            self.assertEqual(msg, '%'.join(('0', '%d' % pin, '1')))
+        # # Test for pin mode INPUT_PULLUP}
+        # self.arduino.Pins[13].Mode.input_pullup()
+        # msg = read()
+        # self.assertEqual(msg, '0%13%2')
 
-            self.Arduino.Connection.write('<AD%02d001>' % pin)
-            msg = self.Arduino.Connection.readline().strip()
-            self.assertEqual(msg, '%'.join(('0','%d' % pin, '1')))
-            sleep(0.5)
+        # # Test for pin on
+        # for pin in (4,7,8,12):
+        #     self.arduino.Pins[pin].Mode.output()
+        #     msg = read()
+        #     self.assertEqual(msg, '%'.join(('0', f'{pin}', '1')))
 
-            # Test for pin off
-            self.Arduino.Connection.write('<AD%02d000>' % pin)
-            msg = self.Arduino.Connection.readline().strip()
-            self.assertEqual(msg, '%'.join(('0', '%d' % pin, '0')))
+        #     self.arduino.Connection.write(f'<AD{pin:02d}001>')
+        #     msg = read()
+        #     self.assertEqual(msg, '%'.join(('0','{pin}', '1')))
+        #     sleep(0.5)
 
-        # Test for invalid command
-        self.Arduino.Connection.write('<óskdjsj>')
-        msg = self.Arduino.Connection.readline().strip()
-        self.assertEqual(msg, 'Invalid command:\xc3\xb3skdjsj')
+        #     # Test for pin off
+        #     self.arduino.Connection.write(f'<AD{pin:02d}000>')
+        #     msg = read()
+        #     self.assertEqual(msg, '%'.join(('0', '{pin}', '0')))
 
-        # Test for memory consumption
-        self.Arduino.get_free_memory()
-        msg = self.Arduino.Connection.readline().strip()
-        aid,action,free = msg.split("%")
-        self.assertEqual('%'.join((aid, action)), '0%free_mem')
+        # # Test for invalid command
+        # self.arduino.Connection.write('<óskdjsj>')
+        # msg = read()
+        # self.assertEqual(msg, 'Invalid command:\xc3\xb3skdjsj')
 
-        # Test for memory consumption
-        self.Arduino.get_firmware_version()
-        msg = self.Arduino.Connection.readline().strip()
-        self.assertEqual(msg, "0%version%0.5.0")
+        # # Test for memory consumption
+        # self.arduino.free_memory()
+        # msg = read()
+        # aid,action,free = msg.split("%")
+        # self.assertEqual('%'.join((aid, action)), '0%free_mem')
 
-        self.Arduino.Connection.close()
+        # # Test for memory consumption
+        # self.arduino.firmware_version()
+        # msg = read()
+        # self.assertEqual(msg, "0%version%0.5.0")
+
+        # self.arduino.Connection.close()
 
         # Test for bus....
 
