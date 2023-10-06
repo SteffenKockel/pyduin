18bba93a5ce5d76c5565c1aa2fc8efc0694f1116
Steffen Kockel <info@steffen-kockel.de>
2023-10-06 18:57:53 +0200

    Add coverage and enable all available tests for pytest
    We want to be able to run a pipeline for pytest and pass it!

--- tests.sh
+++ tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pylint --rcfile tests/pylint.conf $(git ls-files '*.py')
-pytest tests/test_boardfile.py
 yamllint src/pyduin/data/boardfiles/ tests/data/boardfiles/ .github/workflows/
 
+coverage run -m pytest tests/ -v
+coverage report
